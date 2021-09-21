fbe34bd19c318db5c6d00a0edf5ff112dc87fb19
Nikhil Patil <nikhilpatil0022@gmail.com>
2021-09-21 11:00:05 +0530

    Input Formats

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InputFormats from './components/InputFormats/InputFormats';
+
+function App() {
+  return (
+    <div >
+      <InputFormats></InputFormats>
+    </div>
+  );
+}
+
+export default App;
